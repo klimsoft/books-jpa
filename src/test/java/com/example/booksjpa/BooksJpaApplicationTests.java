d7e20043d4ec60f03602357b4a007a401fb76b4b
kamil.klimaszewski@gmail.com <kamil.klimaszewski@gmail.com>
2021-09-15 18:35:02 +0200

    start.spring.io

---
+++ src/test/java/com/example/booksjpa/BooksJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.booksjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
